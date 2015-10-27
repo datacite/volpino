4eb59073f2026d6cf5436061c79dcbc6f0aaf731
Martin Fenner <martin.fenner@datacite.org>
2015-10-28 00:44:56 +0100

    limit uid column to 191 characters for index

--- db/migrate/20151026185035_devise_create_users.rb
+++ db/migrate/20151026185035_devise_create_users.rb
@@ -6,7 +6,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string   :given_names,            limit: 191
       t.string   :email,                  limit: 191
       t.string   :provider,               limit: 255
-      t.string   :uid,                    limit: 255
+      t.string   :uid,                    limit: 191
       t.string   :authentication_token,   limit: 191
       t.string   :role,                   limit: 255, default: 'user'
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 20151026185035) do
     t.string   "given_names",          limit: 191
     t.string   "email",                limit: 191
     t.string   "provider",             limit: 255
-    t.string   "uid",                  limit: 255
+    t.string   "uid",                  limit: 191
     t.string   "authentication_token", limit: 191
     t.string   "role",                 limit: 255, default: "user"
     t.datetime "created_at"
