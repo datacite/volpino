bf05448cd3dec8b1cb11842ee1ab002007369d07
Martin Fenner <martin.fenner@datacite.org>
2015-10-29 20:24:23 +0100

    added application name to header

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -2,11 +2,7 @@ Doorkeeper.configure do
   # Change the ORM that doorkeeper will use (needs plugins)
   orm :active_record
 
-  # This block will be called to check whether the resource owner is authenticated or not.
   resource_owner_authenticator do
-    fail "Please configure doorkeeper resource_owner_authenticator block located in #{__FILE__}"
-    # Put your resource owner authentication logic here.
-    # Example implementation:
     User.find_by_id(session[:user_id]) || redirect_to(new_user_session_url)
   end
 
@@ -75,7 +71,7 @@ Doorkeeper.configure do
   # by default in non-development environments). OAuth2 delegates security in
   # communication to the HTTPS protocol so it is wise to keep this enabled.
   #
-  # force_ssl_in_redirect_uri !Rails.env.development?
+  force_ssl_in_redirect_uri false #!Rails.env.development?
 
   # Specify what grant flows are enabled in array of Strings. The valid
   # strings and the flows they enable are:
