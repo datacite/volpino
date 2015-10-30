2a161d46a4a0614b82a1900fa2c74eb51d37e575
Martin Fenner <martin.fenner@datacite.org>
2015-10-30 11:36:11 +0100

    set log level in production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,9 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  # See everything in the log (default is :info)
+  config.log_level = :debug
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,9 +44,9 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-  # Use the lowest log level to ensure availability of diagnostic information
-  # when problems arise.
-  config.log_level = :debug
+  # See everything in the log (default is :info)
+  log_level = ENV["LOG_LEVEL"] ? ENV["LOG_LEVEL"].to_sym : :info
+  config.log_level = log_level
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
