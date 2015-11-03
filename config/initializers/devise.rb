9730e595f7632dca0f8c0910a08b71ee42dc3ccd
Martin Fenner <martin.fenner@datacite.org>
2015-11-03 16:28:41 +0100

    skip detailed info from omniauth-orcid

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -220,7 +220,8 @@ Devise.setup do |config|
   config.omniauth :orcid, ENV['ORCID_CLIENT_ID'],
                           ENV['ORCID_CLIENT_SECRET'],
                           member: ENV['ORCID_MEMBER'],
-                          sandbox: ENV['ORCID_SANDBOX']
+                          sandbox: ENV['ORCID_SANDBOX'],
+                          skip_info: true
 
   config.omniauth :persona
 
