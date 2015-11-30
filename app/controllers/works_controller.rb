3a020ea925cb9ac213cb6ecc6cbbac7e484c7789
Martin Fenner <martin.fenner@datacite.org>
2015-11-30 23:02:18 +0100

    removed references to work

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,6 +1,6 @@
 class WorksController < ApplicationController
   before_filter :load_user
-  load_and_authorize_resource
+  #load_and_authorize_resource
 
   def index
     @page = params[:page] || 1
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -13,7 +13,6 @@ class Ability
       can [:update, :show], User, :id => user.id
     elsif user.role == "user"
       can [:update, :show], User, :id => user.id
-    else
     end
   end
 end
