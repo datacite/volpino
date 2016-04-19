280af7421684cea83a18ab9720877e65790054a8
Martin Fenner <martin.fenner@datacite.org>
2016-04-19 09:30:21 -0700

    added impactstory badges

--- app/assets/javascripts/users/impactstory.js
+++ app/assets/javascripts/users/impactstory.js
@@ -1,8 +1,7 @@
 /*global d3 */
 
 // construct query string
-var params = d3.select("#api_key"),
-    colors = d3.scale.ordinal().range(["#1abc9c","#ecf0f1","#95a5a6"]);
+var params = d3.select("#api_key");
 
 if (!params.empty()) {
   var contributor_id = params.attr('data-contributor-id').substring(17);
@@ -11,10 +10,45 @@ if (!params.empty()) {
 
 // load the data from the ImpactStory API
 if (query) {
-  d3.json(query)
-    .get(function(error, json) {
-      if (error) { return console.warn(error); }
-      var data = json.badges;
-      console.log(badges)
+  d3.json(query, function(error, json) {
+    if (error) return console.warn(error);
+    BadgesViz(json);
   });
 }
+
+// add data to page
+function BadgesViz(json) {
+  data = json.overview_badges;
+
+  if (typeof data === "undefined" || data.length === 0) { return; }
+
+  d3.select("#impactstory-link")
+    .attr("href", function() { return "https://impactstory.org/u/" + contributor_id; })
+    .text("go to my profile");
+
+  for (var i=0; i<data.length; i++) {
+    var badge = data[i];
+
+    d3.select("#content-impactstory").insert("div")
+      .attr("class", "panel panel-default")
+      .attr("id", "panel-" + i).insert("div")
+      .attr("class", "panel-body")
+      .attr("id", "panel-body-" + i);
+
+    d3.select("#panel-body-" + i).insert("div")
+      .attr("class", "media")
+      .attr("id", "panel-media-" + i).insert("div")
+      .attr("class", "media-left media-badge").insert("img")
+      .attr("src", encodeURI("https://impactstory.org/static/img/badges/" + badge.name + ".png"));
+    d3.select("#panel-media-" + i).insert("div")
+      .attr("class", "media-body")
+      .attr("id", "panel-media-body-" + i).append("h4")
+      .attr("class", "work badge-" + badge.group)
+      .html(badge.display_name);
+
+    d3.select("#panel-media-body-" + i).insert("div")
+      .attr("class", "description")
+      .html(badge.description + " " + badge.context);
+  }
+}
+
