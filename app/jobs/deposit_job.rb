e3bddc7df7902a86307c5a0bc5c1ebb835e7544b
Martin Fenner <martin.fenner@datacite.org>
2016-04-21 21:55:05 -0700

    fixed job syntax

--- app/jobs/deposit_job.rb
+++ app/jobs/deposit_job.rb
@@ -3,7 +3,7 @@ class DepositJob < ActiveJob::Base
 
   def perform(claim)
     ActiveRecord::Base.connection_pool.with_connection do
-      claim.push_lagotto
+      claim.lagotto_post
     end
   end
 end
