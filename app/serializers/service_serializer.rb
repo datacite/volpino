481d43fad8a34e35de5a89177d6ede5c54b17b6c
Martin Fenner <martin.fenner@datacite.org>
2016-08-01 00:27:08 +0200

    added logo_url to services api

--- app/serializers/service_serializer.rb
+++ app/serializers/service_serializer.rb
@@ -1,11 +1,15 @@
 class ServiceSerializer < ActiveModel::Serializer
   cache key: 'service'
-  attributes :id, :title, :logo, :summary, :description, :url, :updated
+  attributes :id, :title, :logo_url, :summary, :description, :url, :updated
 
   has_many :tags
 
   def id
     object.name
+  end
+
+  def logo_url
+    object.image_url
   end
 
   def updated
