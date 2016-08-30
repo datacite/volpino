c484f26a822474b0aa64f53242444c90d180b5b2
Martin Fenner <martin.fenner@datacite.org>
2016-08-30 22:20:59 +0200

    avoid frozen object. Closes #36

--- app/models/user.rb
+++ app/models/user.rb
@@ -146,8 +146,7 @@ class User < ActiveRecord::Base
     items = result.deep_fetch('data', 'orcid_message', 'orcid_profile', 'orcid_activities', 'orcid_works', 'orcid_work') { [] }
 
     items.select do |item|
-      item.extend Hashie::Extensions::DeepFetch
-      item.deep_fetch('source', 'source_orcid', 'path') { nil } == ENV['ORCID_CLIENT_ID']
+      item.fetch('source', {}).fetch('source_orcid', {}).fetch('path', nil) == ENV['ORCID_CLIENT_ID']
     end
   end
 
