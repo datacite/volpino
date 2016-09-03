c5aef1ba73ae6dd315c88050967ef887ee12d0e1
Martin Fenner <martin.fenner@datacite.org>
2016-09-03 11:30:25 +0200

    have default path for redirects

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,7 +3,7 @@ class Users::SessionsController < Devise::SessionsController
 
   # GET /sign_in
   def new
-    store_location_for(:user, request.referer || root_url)
+    store_location_for(:user, request.referer)
     @show_image = true
     super
   end
