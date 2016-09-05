2b178710d5565a313108c41d8f62ead4469f3865
Martin Fenner <martin.fenner@datacite.org>
2016-09-05 08:16:47 +0200

    set cors only in nginx

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -3,31 +3,14 @@ class Api::BaseController < ActionController::Base
   include Authenticable
 
   before_filter :default_format_json,
-                :authenticate_user_from_token!,
-                :cors_preflight_check
-  after_filter :cors_set_access_control_headers, :set_jsonp_format
+                :authenticate_user_from_token!
+  after_filter :set_jsonp_format
 
   # from https://github.com/spree/spree/blob/master/api/app/controllers/spree/api/base_controller.rb
   def set_jsonp_format
     if params[:callback] && request.get?
       self.response_body = "#{params[:callback]}(#{response.body})"
       headers["Content-Type"] = 'application/javascript'
-    end
-  end
-
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-    headers['Access-Control-Max-Age'] = "1728000"
-  end
-
-  def cors_preflight_check
-    if request.method == :options
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
-      headers['Access-Control-Max-Age'] = '1728000'
-      render :text => '', :content_type => 'text/plain'
     end
   end
 
