98cb9909915c7fc2e6eebf5d9233768eb6d2bf36
Martin Fenner <martin.fenner@datacite.org>
2016-11-26 18:59:10 +0100

    added test for adding notification claims to new users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,4 +27,16 @@ describe User, type: :model, vcr: true do
       expect(result.first).to eq("10.5167/UZH-19531")
     end
   end
+
+  describe "claims from notifications" do
+    subject { FactoryGirl.create(:valid_user) }
+
+    let!(:claim) { FactoryGirl.create(:claim, user: subject, orcid: "0000-0001-6528-2027", doi: "10.6084/M9.FIGSHARE.1041821", state: 6) }
+
+    it 'queue_claims_jobs' do
+      expect(subject.claims.count).to eq(1)
+      updated_claim = subject.claims.first
+      expect(updated_claim.human_state_name).to eq("notified")
+    end
+  end
 end
