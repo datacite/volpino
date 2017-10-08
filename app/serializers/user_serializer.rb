0f5e084506b089db32f23939008daa058c23b050
Martin Fenner <mfenner@datacite.org>
2017-10-08 09:55:41 +0200

    use name instead of credit_name

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   cache key: 'user'
 
-  attributes :given_names, :family_name, :credit_name, :orcid, :github, :created, :updated
+  attributes :given_names, :family_name, :name, :orcid, :github, :created, :updated
   attribute :email, if: :can_read
   attribute :provider_id, if: :can_read
   attribute :client_id, if: :can_read
