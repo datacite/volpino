23e4c1b34631ba5d5c67e1ed56de10c055e17972
Martin Fenner <mfenner@datacite.org>
2017-12-11 11:43:42 +0100

    add default signout path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource_or_scope)
-    request.referrer
+    request.referrer || root_path
   end
 
   #convert parameters with hyphen to parameters with underscore.
