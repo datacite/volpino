fc3ef4551fe1e433672d60826387493424843e65
Martin Fenner <martin.fenner@datacite.org>
2018-10-06 11:37:48 +0200

    update factory syntax

--- spec/factories/default.rb
+++ spec/factories/default.rb
@@ -1,33 +1,33 @@
 FactoryBot.define do
   factory :user do
     sequence(:name) { |n| "Josiah Carberry{n}" }
-    provider "orcid"
-    role_id "user"
+    provider { "orcid" }
+    role_id { "user" }
     sequence(:uid) { |n| "0000-0002-1825-000#{n}" }
 
     factory :admin_user do
-      role_id "staff_admin"
-      uid "0000-0002-1825-0003"
+      role_id { "staff_admin" }
+      uid { "0000-0002-1825-0003" }
     end
 
     factory :staff_user do
-      role_id "staff_user"
-      uid "0000-0002-1825-0004"
+      role_id { "staff_user" }
+      uid { "0000-0002-1825-0004" }
     end
 
     factory :regular_user do
-      role_id "user"
-      uid "0000-0002-1825-0001"
+      role_id { "user" }
+      uid { "0000-0002-1825-0001" }
     end
 
     factory :valid_user do
-      uid '0000-0001-6528-2027'
-      authentication_token ENV['ACCESS_TOKEN']
+      uid { '0000-0001-6528-2027' }
+      authentication_token { ENV['ACCESS_TOKEN'] }
     end
 
     factory :invalid_user do
-      uid '0000-0001-6528-2027'
-      authentication_token nil
+      uid { '0000-0001-6528-2027' }
+      authentication_token { nil }
     end
 
     initialize_with { User.where(uid: uid).first_or_initialize }
@@ -35,9 +35,9 @@ FactoryBot.define do
 
   factory :claim do
     uuid { SecureRandom.uuid }
-    orcid "0000-0002-1825-0001"
-    doi "10.5061/DRYAD.781PV"
-    source_id "orcid_update"
+    orcid { "0000-0002-1825-0001" }
+    doi { "10.5061/DRYAD.781PV" }
+    source_id { "orcid_update" }
 
     # association :user, factory: :user
 
