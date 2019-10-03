d4d37bf97a4ab9724264a93a8351ac391e2b4e7d
Martin Fenner <martin.fenner@datacite.org>
2019-10-03 11:59:04 +0200

    use correct key in graphql

--- app/graphql/types/person_type.rb
+++ app/graphql/types/person_type.rb
@@ -5,7 +5,7 @@ class PersonType < BaseObject
 
   field :id, ID, null: true, description: "The ORCID ID of the person."
   field :name, String, null: true, description: "The name of the person."
-  field :given_name, String, null: true, description: "Given name. In the U.S., the first name of a Person."
+  field :given_name, String, null: true, hash_key: "given_names", description: "Given name. In the U.S., the first name of a Person."
   field :family_name, String, null: true, description: "Family name. In the U.S., the last name of an Person."
 
   def id
