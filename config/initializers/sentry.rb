54104504daf723873717a66766f9d63083a6533b
Martin Fenner <martin.fenner@datacite.org>
2019-10-07 10:10:47 +0200

    handle sentry errors

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,4 +2,8 @@ Raven.configure do |config|
   config.dsn = ENV["SENTRY_DSN"]
   config.release = "volpino:" + Volpino::Application::VERSION
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+  config.transport_failure_callback = lambda { |event|
+    logger = Logger.new(STDOUT)
+    logger.error "[Sentry Error]: " + event.inspect
+  }
 end
