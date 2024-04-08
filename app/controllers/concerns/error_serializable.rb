aaf08b53dbe4505d891c83b660a05a02091f16b6
Richard Hallett <richard@richardhallett.com>
2024-04-08 16:57:49 +0200

    Fix warning for errors

--- app/controllers/concerns/error_serializable.rb
+++ app/controllers/concerns/error_serializable.rb
@@ -8,7 +8,7 @@ module ErrorSerializable
       return nil if errors.nil?
 
       arr = Array.wrap(errors).reduce([]) do |sum, err|
-        source = err.keys.first
+        source = err.attibute_names.first
 
         Array.wrap(err.values.first).each do |title|
           sum << { source: source, title: title.is_a?(String) ? title.capitalize : title.to_s }
