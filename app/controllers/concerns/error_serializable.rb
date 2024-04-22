f833acdd3ea0f5b15ad64de4097485c077b182c7
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2024-04-22 14:24:28 +0200

    rework error serializer due to deprecation warning

--- app/controllers/concerns/error_serializable.rb
+++ app/controllers/concerns/error_serializable.rb
@@ -4,20 +4,25 @@ module ErrorSerializable
   extend ActiveSupport::Concern
 
   included do
-    def serialize_errors(errors)
+    def serialize_errors(errors, options = {})
       return nil if errors.nil?
 
-      arr = Array.wrap(errors).reduce([]) do |sum, err|
-        source = err.keys.first
+      errors_arr = []
 
-        Array.wrap(err.values.first).each do |title|
-          sum << { source: source, title: title.is_a?(String) ? title.capitalize : title.to_s }
+      errors.each do |err|
+        unless errors_arr.any? { |e| e[:source] == err.attribute }
+          new_err = { source: err.attribute, title: capitalize_error_message(err) }
+          new_err[:uid] = options[:uid] if options[:uid].present?
+          errors_arr << new_err
         end
-
-        sum
       end
 
-      { errors: arr }.to_json
+      { errors: errors_arr }.to_json
     end
   end
+
+  private
+    def capitalize_error_message(error)
+      error.message.sub(/^./, &:upcase)
+    end
 end
