45ff59448a9e45ae3636aa8070313781fe514344
Wendel Fabian Chinsamy <wendel.chinsamy@datacite.org>
2024-04-29 12:11:01 +0200

    fix spec

--- spec/jobs/user_job_spec.rb
+++ spec/jobs/user_job_spec.rb
@@ -5,8 +5,7 @@ require "rails_helper"
 RSpec.describe UserJob, type: :job, elasticsearch: true do
   it "enqueue jobs" do
     perform_enqueued_jobs do
-      user = FactoryBot.create(:valid_user)
-      job = user.queue_user_job
+      user = FactoryBot.create(:valid_user).queue_user_job
       expect(UserJob).to(have_been_enqueued.at_least(:once))
     end
   end
@@ -14,8 +13,7 @@ RSpec.describe UserJob, type: :job, elasticsearch: true do
   it "executes perform", vcr: true do
     perform_enqueued_jobs do
       expect(user.claims.count).to eq(0)
-      user = FactoryBot.create(:valid_user)
-      job = user.queue_user_job
+      user = FactoryBot.create(:valid_user).queue_user_job
       expect(user.claims.count).to eq(23)
     end
   end
