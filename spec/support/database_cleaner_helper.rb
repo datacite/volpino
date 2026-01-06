1879187e2c1ad1a3eb9ac3e5d53fe19cd33ca836
kudakwashe SIziva <kuda.siziva@datacite.org>
2026-01-06 13:48:19 +0100

    consolidate database cleaner config

--- spec/support/database_cleaner_helper.rb
+++ spec/support/database_cleaner_helper.rb
@@ -2,22 +2,22 @@
 
 RSpec.configure do |config|
   config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner[:active_record].clean_with(:truncation)
   end
 
   config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner[:active_record].strategy = :transaction
   end
 
   config.before(:each, js: true) do
-    DatabaseCleaner.strategy = :truncation, { pre_count: true }
+    DatabaseCleaner[:active_record].strategy = :truncation, { pre_count: true }
   end
 
   config.before(:each) do
-    DatabaseCleaner.start
+    DatabaseCleaner[:active_record].start
   end
 
   config.after(:each) do
-    DatabaseCleaner.clean
+    DatabaseCleaner[:active_record].clean
   end
 end
